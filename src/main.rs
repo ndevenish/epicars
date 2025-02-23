96d3b3a7ea4b56d31bfe29b0d6b2e78316ff65e6
Nicholas Devenish <ndevenish@gmail.com>
2025-02-23 20:23:10 +0000

    Move to multithreaded tokio
    I don't intend to use it, but it should be thread safe and this helps
    ensure that

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::time::Duration;
 
 use epics::server::Server;
 
-#[tokio::main(flavor = "current_thread")]
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() {
     Server::new(5065).await.unwrap();
     println!("Entering main() infinite loop");
