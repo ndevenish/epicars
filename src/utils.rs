c8624ca998fec964f198f56aae660120f36c2c33
Nicholas Devenish <ndevenish@gmail.com>
2025-10-19 00:35:37 +0100

    Set SO_REUSEADDR on reusable port

--- src/utils.rs
+++ src/utils.rs
@@ -12,6 +12,7 @@ use tracing::{debug, warn};
 pub(crate) fn new_reusable_udp_socket<T: ToSocketAddrs>(address: T) -> io::Result<UdpSocket> {
     let socket = socket2::Socket::new(Domain::IPV4, Type::DGRAM, Some(Protocol::UDP))?;
     socket.set_reuse_port(true)?;
+    socket.set_reuse_address(true)?;
     socket.set_nonblocking(true)?;
     let addr = address.to_socket_addrs()?.next().unwrap();
     socket.bind(&addr.into())?;
